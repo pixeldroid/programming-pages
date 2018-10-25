52b624df401929225be731d32e2f4242a25e882e
ellemenno <ellemenno@users.noreply.github.com>
2018-10-25 19:47:31 -0400

    use remote theme (remove theme files from docs/)

--- docs/_includes/scripts/example.js
+++
@@ -1,7 +0,0 @@
-example_code_tabs_init = function() {
-  $('#example-code-tabs .item')
-  .tab()
-  ;
-};
-
-$(example_code_tabs_init);
--- docs/_includes/styles/semantic-ui/_version.css
+++
@@ -1,5 +0,0 @@
-/*!
-* Semantic UI 2.3.0
-* http://github.com/semantic-org/semantic-ui/
-* http://opensource.org/licenses/MIT
-*/
