dabfe4a31d8d30c64cdb6fa6300b54764949137f
ellemenno <ellemenno@users.noreply.github.com>
2019-01-22 21:17:38 -0500

    implement 'scroll into view' for active items in the indices list

--- _includes/scripts/indices.js
+++ _includes/scripts/indices.js
@@ -1,7 +1,17 @@
+set_indices_scrolltop = function() {
+  // look for active item and scroll it into view (set by render_indices.liquid)
+  var active_item = $('#active-page-index').first();
+  if (active_item.length < 1) return;
+
+  var y = active_item.position().top;
+  $('#sidebar-fixed').scrollTop(y);
+}
+
 collection_tabs_init = function() {
   $('#collection-tabs .item').tab({
     history: true,
     historyType: 'hash',
+    onFirstLoad: set_indices_scrolltop,
   });
 
   // set up shortut key sequences to change tabs
